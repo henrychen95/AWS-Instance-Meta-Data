f6c82bf009a2b2a82b9d4d5009917098175055a7
henrychen95 <yongwei.chen@gmail.com>
2013-08-30 00:06:39 +0800

    Revert "First version"
    This reverts commit 89a825332070aa701fa6a00ae505390dea689df7.

--- show_index.php
+++
@@ -1,5 +0,0 @@
-<?php
-header('Content-type: text/html');
-$code = file_get_contents("index.php");
-highlight_string($code);
-include_once("analyticstracking.php");
--- show_index_bak.php
+++
@@ -1,4 +0,0 @@
-<?php
-header('Content-type: text/html');
-$code = file_get_contents("index_bak.php");
-highlight_string($code);
